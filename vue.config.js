997f603b03fd069fd1a196ff2082f97206db90c6
yete <474026268@qq.com>
2020-07-10 17:43:46 +0800

    添加vue.config.js配置文件

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+module.exports = {
+
+
+
+
+    // css相关配置
+    css: {
+        // 是否使用css分离插件 ExtractTextPlugin
+        extract: true,
+        // 开启 CSS source maps?
+        sourceMap: false,
+        // css预设器配置项
+        loaderOptions: {
+            sass: {
+                prependData: `@import"./src/styles/main.scss";`
+            }
+        },
+        // 启用CSS modules for all cs / pre-processor files
+        requireModuleExtension: false
+    }
+}
